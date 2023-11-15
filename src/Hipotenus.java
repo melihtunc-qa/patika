ed13d4973c24ea21abc8e97892451a0fc2aa5d20
bronze2727 <melihtunc27@gmail.com>
2023-11-15 21:47:59 +0300

    first

---
+++ src/Hipotenus.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Hipotenus {
+
+    public static void main(String[] args) {
+
+
+
+    int a,b;
+
+    double c;
+
+    Scanner input = new Scanner(System.in);
+
+        System.out.println("1. Kenarı giriniz");
+
+        a = input.nextInt();
+
+        System.out.println("2. Kenarı giriniz");
+
+        b=input.nextInt();
+
+        c=Math.sqrt((a*a)+(b*b));
+
+        System.out.println("Hipotenüs : " + c);
+
+
+    }
+
+}
