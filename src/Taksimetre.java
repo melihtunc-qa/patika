4b9eaab2bcb79c0fa3d5f4a55cadc88af66514b3
bronze2727 <melihtunc27@gmail.com>
2023-11-15 22:00:56 +0300

    second

---
+++ src/Taksimetre.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Taksimetre {
+
+    public static void main(String[] args) {
+
+
+    int acilis_ücreti = 30;
+
+    double km_degeri,taksimetre_tutarı;
+
+        System.out.println("kilometre giriniz ");
+
+    Scanner input = new Scanner(System.in);
+
+    km_degeri = input.nextDouble();
+
+    taksimetre_tutarı = acilis_ücreti + km_degeri*20;
+        System.out.println("taksimetre tutarı : " + taksimetre_tutarı);
+
+
+
+    }
+}
