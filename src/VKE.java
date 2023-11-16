27aa9c54a16f10a43f8dc0d1ced81c779e75d7f6
bronze2727 <melihtunc27@gmail.com>
2023-11-16 23:32:04 +0300

    Kitle Endeksi

---
+++ src/VKE.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VKE {
+    public static void main(String[] args) {
+
+        double kilogram,boy;
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Lütfen boyunuzu giriniz");
+        boy=input.nextDouble();
+
+        System.out.println("Lütfen kilonuzu giriniz");
+
+        kilogram=input.nextDouble();
+
+       double vke=kilogram/(boy*boy);
+
+        System.out.println("boyunuz : " + boy + "kilonuz : " + kilogram );
+        System.out.println("Kitle endeksiniz : " + vke);
+
+
+
+
+    }
+}
