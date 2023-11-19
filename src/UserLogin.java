5042872df0ddf7826d95512d3a810a1c86524bf4
bronze2727 <melihtunc27@gmail.com>
2023-11-19 23:30:23 +0300

    UserLogin

---
+++ src/UserLogin.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class UserLogin {
+
+    public static void main(String[] args) {
+
+        String username,password;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Kullanıcı adınız : ");
+
+        username = input.nextLine();
+
+        System.out.println("şifreniz : ");
+
+        password= input.nextLine();
+
+
+        if (username.equals("patika")&&password.equals("java123")){
+
+            System.out.println("Giriş yaptınız");
+
+        }else {
+            System.out.println("Bilgileriniz yanlış");
+        }
+
+    }
+}
