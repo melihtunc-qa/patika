c14bfee5277df36551b80c8ff4cb0c426b191b52
bronze2727 <melihtunc27@gmail.com>
2023-11-19 23:44:19 +0300

    Air

---
+++ src/Arrangement.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Arrangement {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("birinci. sayı :");
+
+        a= input.nextInt();;
+
+        System.out.println("ikinci sayı : ");
+
+        b=input.nextInt();
+
+        System.out.println("üçüncü sayı");
+
+        c=input.nextInt();
+
+
+        if (a>b&&a>c){
+            System.out.println("birinci sayı en büyüktür");
+
+        } else if (b>a&&b>c) {
+            System.out.println("ikinci sayı en büyüktür");
+        }else {
+            System.out.println("üçüncü sayı en büyüktür");
+        }
+
+    }
+}
