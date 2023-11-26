befd8c3c02cc3a30537253da52912d3068083465
bronze2727 <melihtunc27@gmail.com>
2023-11-26 20:38:41 +0300

    Kuvvet Bulma

---
+++ src/training/Kuvvet.java
@@ -0,0 +1,36 @@
+package training;
+
+import java.util.Scanner;
+
+public class Kuvvet {
+
+
+    public static void main(String[] args) {
+
+
+
+        int n;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Bir sayı giriniz");
+        n = sc.nextInt();
+
+        for (int i = 1 ; i<=n ; i*=4){
+
+            System.out.println("Dördün katı : " + i);
+
+
+        }
+
+        for (int i =1;i<=n;i*=5){
+            System.out.println("Beşin katı : " + i);
+        }
+
+
+
+
+    }
+
+
+}
