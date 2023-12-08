0b9875e8ca4f95fb711a923efbe004d2370c6cdb
bronze2727 <melihtunc27@gmail.com>
2023-12-08 17:11:46 +0300

    RecursiveFibonacci

---
+++ src/examples/RecursiveFibonacci.java
@@ -0,0 +1,19 @@
+package examples;
+
+public class RecursiveFibonacci {
+
+    static int fib(int n){
+
+        if (n==1||n==2){
+            return 1;
+        }
+
+        return fib(n-1)+fib(n-2);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
