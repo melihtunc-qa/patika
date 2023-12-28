f280b815fc4a5cccbe54add9956975458f5006ec
Melih TUNÇ <115929641+melihtunc-qa@users.noreply.github.com>
2023-12-28 23:34:21 +0300

    Update odev2.sql

--- rental.sql
+++ rental.sql
@@ -6,3 +6,14 @@ select * from film where rental_rate=0.99 and (replacement_cost=12.99 or replace
 
 select * from film where length<50 and not ( rental_rate=2.99 or rental_rate=4.99);
 
+---odev2----
+
+
+select * from film where replacement_cost between 12.99 and 16.99;
+
+select first_name,last_name from actor where first_name in ('Penelope','Nick','Ed');
+
+select * from film where (rental_rate in(0.99,2.99,4.99)) and (replacement_cost in (12.99,15.99,28.99));
+
+
+
